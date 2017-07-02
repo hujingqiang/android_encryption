347751e417c62038ce003b71286a72c485825e4d
hujingqiang <hujingqiang@yahoo.com>
2017-07-02 21:06:10 +0800

    Initial commit

---
+++ app/src/main/java/net/keepbalance/encryption_android/MainActivity.java
@@ -0,0 +1,70 @@
+package net.keepbalance.encryption_android;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+                try {
+                    CryptLib _crypt = new CryptLib();
+                    String output= "";
+                    String plainText = "This is the text to be encrypted.";
+                    String key = CryptLib.SHA256("my secret key", 32); //32 bytes = 256 bit
+                    String iv ="4NAfcTL5nWERGSLl"; //CryptLib.generateRandomIV(16); //16 bytes = 128 bit
+
+                    System.out.println("key=" + key);
+                    System.out.println("iv=" + iv);
+                    output = _crypt.encrypt(plainText, key, iv); //encrypt
+                    System.out.println("encrypted text=" + output);
+                    output = _crypt.decrypt(output, key,iv); //decrypt
+                    System.out.println("decrypted text=" + output);
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
